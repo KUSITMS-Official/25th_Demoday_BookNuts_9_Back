90bd175c31c9163535ddd326d246b4c729b086be
jaejeong <78673570+jaejlf@users.noreply.github.com>
2022-04-29 00:46:37 +0900

    Project Initial Setting

---
+++ src/main/java/team/nine/booknutsbackend/BooknutsBackendApplication.java
@@ -0,0 +1,13 @@
+package team.nine.booknutsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooknutsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooknutsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/team/nine/booknutsbackend/BooknutsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package team.nine.booknutsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooknutsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
