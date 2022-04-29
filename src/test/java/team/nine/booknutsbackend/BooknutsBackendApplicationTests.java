67b466611c0330c14bf5c2f3f05cdf7323d0a0f7
jaejeong <78673570+jaejlf@users.noreply.github.com>
2022-04-29 16:51:38 +0900

    ✅ [MOD] gradlew build - test 코드 삭제

--- src/test/java/team/nine/booknutsbackend/BooknutsBackendApplicationTests.java
+++ src/test/java/team/nine/booknutsbackend/BooknutsBackendApplicationTests.java
@@ -3,7 +3,7 @@ package team.nine.booknutsbackend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class BooknutsBackendApplicationTests {
 
 	@Test
