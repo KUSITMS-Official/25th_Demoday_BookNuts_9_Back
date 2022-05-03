5ca83df3dd6f9dd1af44591e64be90cbb8b0f97a
jaejeong <78673570+jaejlf@users.noreply.github.com>
2022-05-04 04:34:30 +0900

    ✅ [MOD] Common, User Exception 처리

--- src/main/java/team/nine/booknutsbackend/exception/CustomExceptionHandler.java
+++ src/main/java/team/nine/booknutsbackend/exception/CustomExceptionHandler.java
@@ -7,39 +7,30 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import team.nine.booknutsbackend.exception.user.NotFoundEmailException;
 import team.nine.booknutsbackend.exception.user.PasswordErrorException;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
 
 @RestControllerAdvice
 public class CustomExceptionHandler {
 
     /**
-     * Common Exception Handler
+     * Common 예외 처리
      **/
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Object> CommonExceptionHandler(Exception e) {
-        return new ResponseEntity<>(responseFormat(e), HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(ExceptionResponse.format(e), HttpStatusMap.getCode(e));
     }
 
     /**
-     * 로그인 Exception
+     * User 관련 예외 처리
      **/
     // 가입되지 않은 이메일
     @ExceptionHandler(NotFoundEmailException.class)
     public ResponseEntity<Object> notFoundEmailHandler(NotFoundEmailException e) {
-        return new ResponseEntity<>(responseFormat(e), HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(ExceptionResponse.format(e), HttpStatus.NOT_FOUND);
     }
 
     //잘못된 비밀번호
     @ExceptionHandler(PasswordErrorException.class)
-    public ResponseEntity<Object> notFoundEmailHandler(PasswordErrorException e) {
-        return new ResponseEntity<>(responseFormat(e), HttpStatus.BAD_REQUEST);
-    }
-
-    private Object responseFormat(Exception e) {
-        Map<String, String> map = new LinkedHashMap<>();
-        map.put("error", e.getClass().getName());
-        map.put("msg", e.getMessage());
-        return map;
+    public ResponseEntity<Object> passwordErrorHandler(PasswordErrorException e) {
+        return new ResponseEntity<>(ExceptionResponse.format(e), HttpStatus.BAD_REQUEST);
     }
 }
