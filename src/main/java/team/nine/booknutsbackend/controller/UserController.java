445734f708d4ba62cc3674682a67589d7b7fe588
jaejeong <78673570+jaejlf@users.noreply.github.com>
2022-05-04 17:22:42 +0900

    ✅ [MOD] Service, Repository 계층 분리

--- src/main/java/team/nine/booknutsbackend/controller/UserController.java
+++ src/main/java/team/nine/booknutsbackend/controller/UserController.java
@@ -3,20 +3,16 @@ package team.nine.booknutsbackend.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import team.nine.booknutsbackend.config.JwtTokenProvider;
 import team.nine.booknutsbackend.domain.User;
-import team.nine.booknutsbackend.exception.user.NotFoundEmailException;
 import team.nine.booknutsbackend.exception.user.PasswordErrorException;
-import team.nine.booknutsbackend.repository.UserRepository;
 import team.nine.booknutsbackend.service.UserService;
 
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.NoSuchElementException;
 
 @RequiredArgsConstructor
 @RestController
@@ -27,7 +23,6 @@ public class UserController {
 
     private final PasswordEncoder passwordEncoder;
     private final JwtTokenProvider jwtTokenProvider;
-    private final UserRepository userRepository;
 
     //회원가입
     @PostMapping("/join")
@@ -61,8 +56,7 @@ public class UserController {
     //로그인
     @PostMapping("/login")
     public ResponseEntity<Object> login(@RequestBody Map<String, String> user) {
-        User loginUser = userRepository.findByEmail(user.get("email"))
-                .orElseThrow(() -> new NotFoundEmailException("가입되지않은 이메일입니다."));
+        User loginUser = userService.loadUserByUsername(user.get("email"));
 
         if (!passwordEncoder.matches(user.get("password"), loginUser.getPassword())) {
             throw new PasswordErrorException("잘못된 비밀번호입니다.");
@@ -79,16 +73,15 @@ public class UserController {
     @PostMapping("/userinfo")
     public ResponseEntity<Object> curUser(@RequestBody Map<String, String> userToken) {
         String email = jwtTokenProvider.getUserPk(userToken.get("token"));
-        UserDetails loginUser = userService.loadUserByUsername(email);
+        User curUser = userService.loadUserByUsername(email);
 
-        return new ResponseEntity<>(loginUser, HttpStatus.OK);
+        return new ResponseEntity<>(curUser, HttpStatus.OK);
     }
 
     // 현재 유저 정보 - id로 조회
     @GetMapping("/userinfo/{id}")
     public ResponseEntity<Object> curUserInfo(@PathVariable String id) {
-        User curUser = userRepository.findById(Long.parseLong(id))
-                .orElseThrow(() -> new NoSuchElementException("존재하지 않는 유저의 아이디입니다."));
+        User curUser = userService.findUserById(Long.parseLong(id));
 
         return new ResponseEntity<>(curUser, HttpStatus.OK);
     }
--- src/main/java/team/nine/booknutsbackend/service/UserService.java
+++ src/main/java/team/nine/booknutsbackend/service/UserService.java
@@ -1,7 +1,6 @@
 package team.nine.booknutsbackend.service;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
@@ -9,18 +8,13 @@ import org.springframework.transaction.annotation.Transactional;
 import team.nine.booknutsbackend.domain.User;
 import team.nine.booknutsbackend.repository.UserRepository;
 
+import java.util.NoSuchElementException;
+
 @RequiredArgsConstructor
 @Service
 public class UserService implements UserDetailsService {
 
     private final UserRepository userRepository;
-
-    //유저 정보 조회
-    @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        return userRepository.findByEmail(email)
-                .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
-    }
 
     //유저 닉네임 중복 체크
     @Transactional(readOnly = true)
@@ -35,7 +29,22 @@ public class UserService implements UserDetailsService {
     }
 
     //회원가입
+    @Transactional
     public User join(User newUser) {
         return userRepository.save(newUser);
+    }
+
+    //이메일로 유저 정보 조회
+    @Override
+    public User loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("가입되지 않은 이메일입니다."));
+    }
+
+    // ID로 유저 정보 조회
+    @Transactional(readOnly = true)
+    public User findUserById(Long id){
+        return userRepository.findById(id)
+                .orElseThrow(() -> new NoSuchElementException("존재하지 않는 유저의 아이디입니다."));
     }
 }
