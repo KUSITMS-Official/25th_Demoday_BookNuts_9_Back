792a4cb7917947c755588504870416536a0ead8e
jaejeong <78673570+jaejlf@users.noreply.github.com>
2022-05-06 18:21:15 +0900

    ✅ [MOD] 현재 유저 정보 조회 - X-AUTH-TOKEN 조회 방식으로 변경

---
+++ src/main/java/team/nine/booknutsbackend/config/interceptor/JWTAuthConfig.java
@@ -0,0 +1,20 @@
+package team.nine.booknutsbackend.config.interceptor;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class JWTAuthConfig implements WebMvcConfigurer {
+
+    private final JWTAuthInterceptor jwtAuthInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(jwtAuthInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/auth/join", "/auth/login"); //예외 처리할 주소
+    }
+}
---
+++ src/main/java/team/nine/booknutsbackend/config/interceptor/JWTAuthInterceptor.java
@@ -0,0 +1,31 @@
+package team.nine.booknutsbackend.config.interceptor;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import team.nine.booknutsbackend.config.JwtTokenProvider;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+@RequiredArgsConstructor
+public class JWTAuthInterceptor implements HandlerInterceptor {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    //컨트롤러 실행 전 수행 (true -> 컨트롤러로 진입, false -> 진입 X)
+    @Override
+    public boolean preHandle(HttpServletRequest request,
+                             HttpServletResponse response, Object handler) throws Exception {
+
+        String token = jwtTokenProvider.resolveToken(request);
+
+//        // 테스트 위해 임시로 주석 처리
+//        if (!jwtTokenProvider.validateToken(token)) {
+//            throw new Exception("유효하지 않은 X-AUTH-TOKEN");
+//        }
+
+        return true;
+    }
+}
