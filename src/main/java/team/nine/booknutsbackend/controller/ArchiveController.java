ffa1780e19efe4e90cea3bf19facb69751ebc540
JoSunHee <82442788+JoSunHee@users.noreply.github.com>
2022-05-20 04:24:57 +0900

    ✨ [FEAT] 아카이브 생성 및 추가

--- src/main/java/team/nine/booknutsbackend/controller/ArchiveController.java
+++ src/main/java/team/nine/booknutsbackend/controller/ArchiveController.java
@@ -3,23 +3,15 @@ package team.nine.booknutsbackend.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
-import team.nine.booknutsbackend.domain.Series.Series;
-import team.nine.booknutsbackend.domain.Series.SeriesBoard;
 import team.nine.booknutsbackend.domain.User;
-import team.nine.booknutsbackend.domain.archive.Archive;
 import team.nine.booknutsbackend.dto.Request.ArchiveRequest;
-import team.nine.booknutsbackend.dto.Request.SeriesRequest;
 import team.nine.booknutsbackend.dto.Response.ArchiveResponse;
 import team.nine.booknutsbackend.dto.Response.BoardResponse;
-import team.nine.booknutsbackend.exception.archive.ArchiveNotFoundException;
-import team.nine.booknutsbackend.exception.series.SeriesNotFoundException;
 import team.nine.booknutsbackend.service.ArchiveService;
 import team.nine.booknutsbackend.service.UserService;
 
 import java.security.Principal;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -40,13 +32,13 @@ public class ArchiveController {
     }
 
     //아카이브 생성
-    @PostMapping("/creatarchive")
+    @PostMapping("/createarchive")
     public ResponseEntity<Object> createArchive(@RequestBody ArchiveRequest archiveRequest, Principal principal) {
         User user = userService.loadUserByUsername(principal.getName());
         archiveService.saveArchive(archiveRequest, user);
 
         Map<String, String> map = new HashMap<>();
-        map.put("result", "추가 완료");
+        map.put("result", "아카이브 생성 완료");
         return new ResponseEntity<>(map, HttpStatus.OK);
     }
 
@@ -55,5 +47,16 @@ public class ArchiveController {
     public List<BoardResponse> findArchive(@PathVariable Long archiveId, Principal principal) {
         User user = userService.loadUserByUsername(principal.getName());
         return archiveService.findArchive(archiveId, user);
+    }
+
+    //아카이브에 추가
+    @GetMapping("/addarchive/{archiveId}/{boardId}")
+    public ResponseEntity<Object> addToArchive(@PathVariable Long archiveId, @PathVariable Long boardId, Principal principal) {
+        User user = userService.loadUserByUsername(principal.getName());
+        archiveService.addToArchive(archiveId, boardId, user);
+
+        Map<String, String> map = new HashMap<>();
+        map.put("result", "아카이브에 추가 완료");
+        return new ResponseEntity<>(map, HttpStatus.OK);
     }
 }
--- src/main/java/team/nine/booknutsbackend/service/ArchiveService.java
+++ src/main/java/team/nine/booknutsbackend/service/ArchiveService.java
@@ -3,20 +3,18 @@ package team.nine.booknutsbackend.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import team.nine.booknutsbackend.domain.Series.Series;
-import team.nine.booknutsbackend.domain.Series.SeriesBoard;
+import team.nine.booknutsbackend.domain.Board;
 import team.nine.booknutsbackend.domain.User;
 import team.nine.booknutsbackend.domain.archive.Archive;
 import team.nine.booknutsbackend.domain.archive.ArchiveBoard;
 import team.nine.booknutsbackend.dto.Request.ArchiveRequest;
-import team.nine.booknutsbackend.dto.Request.SeriesRequest;
 import team.nine.booknutsbackend.dto.Response.ArchiveResponse;
 import team.nine.booknutsbackend.dto.Response.BoardResponse;
-import team.nine.booknutsbackend.dto.Response.SeriesResponse;
 import team.nine.booknutsbackend.exception.archive.ArchiveNotFoundException;
-import team.nine.booknutsbackend.exception.series.SeriesNotFoundException;
+import team.nine.booknutsbackend.exception.board.BoardNotFoundException;
 import team.nine.booknutsbackend.repository.ArchiveBoardRepository;
 import team.nine.booknutsbackend.repository.ArchiveRepository;
+import team.nine.booknutsbackend.repository.BoardRepository;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,6 +25,7 @@ public class ArchiveService {
 
     private final ArchiveRepository archiveRepository;
     private final ArchiveBoardRepository archiveBoardRepository;
+    private final BoardRepository boardRepository;
     private final BoardService boardService;
 
     //아카이브 리스트 조회
@@ -41,15 +40,10 @@ public class ArchiveService {
         return archiveResponseList;
     }
 
-    //아카이브 추가
+    //아카이브 생성
     public void saveArchive(ArchiveRequest archiveRequest, User user) {
         Archive archive = ArchiveRequest.newArchive(archiveRequest, user);
         archiveRepository.save(archive);
-
-//        ArchiveBoard archiveBoard = new ArchiveBoard();
-//        archiveBoard.setArchive(archive);
-//        archiveBoard.setBoard(boardService.findBoard(archiveRequest.getBoardId()));
-//        archiveBoardRepository.save(archiveBoard);
     }
 
     //특정 아카이브 조회
@@ -64,5 +58,18 @@ public class ArchiveService {
             boardList.add(BoardResponse.boardResponse(archiveBoard.getBoard(), user));
         }
         return boardList;
+    }
+
+    //아카이브에 추가
+    public void addToArchive(Long archiveId, Long boardId, User user) {
+        Archive archive = archiveRepository.findById(archiveId)
+                .orElseThrow(() -> new ArchiveNotFoundException("존재하지 않는 아카이브 아이디입니다."));
+        Board board = boardRepository.findById(boardId)
+                .orElseThrow(() -> new BoardNotFoundException("존재하지 않는 게시글 번호입니다."));
+
+        ArchiveBoard archiveBoard = new ArchiveBoard();
+        archiveBoard.setArchive(archive);
+        archiveBoard.setBoard(board);
+        archiveBoardRepository.save(archiveBoard);
     }
 }
